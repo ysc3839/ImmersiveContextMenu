433341037caa5bb3673a99553c9b75b768607384
Richard Yu <yurichard3839@gmail.com>
2020-07-26 22:49:24 +0800

    Add more menu item

--- resource.h
+++ resource.h
@@ -30,6 +30,11 @@
 #define ID_SORTBY_ITEMTYPE              32788
 #define ID_SORTBY_DATEMODIFIED          32789
 #define ID_TEST_OPENINVISUALSTUDIO      32790
+#define ID_TEST_NEW                     32791
+#define ID_TEST_DISPLAYSETTINGS         32792
+#define ID_TEST_PERSONALIZE             32793
+#define ID_NEW_FOLDER                   32794
+#define ID_Menu                         32795
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -38,7 +43,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32791
+#define _APS_NEXT_COMMAND_VALUE         32796
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
